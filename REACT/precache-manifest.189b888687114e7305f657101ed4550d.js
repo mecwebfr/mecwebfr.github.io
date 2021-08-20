1dc76bc880d9a1b7a7c3ab8e7469163b2d4e0a68
webgast <info@webgast.nl@users.noreply.github.com>
2021-08-20 13:04:11 +0200

    resources toegevoegd

---
+++ REACT/precache-manifest.189b888687114e7305f657101ed4550d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8568888de8cb65c2ab4aa857002654",
+    "url": "./index.html"
+  },
+  {
+    "revision": "228d48eaa8e49df494e1",
+    "url": "./static/css/main.eef0c8f0.chunk.css"
+  },
+  {
+    "revision": "0f94f36a2b669bcce86f",
+    "url": "./static/js/2.4ae9f365.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "./static/js/2.4ae9f365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228d48eaa8e49df494e1",
+    "url": "./static/js/main.277b2c23.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
